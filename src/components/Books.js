0ecc0c8cb11f5f555219741ec23b1dada9217c57
unknown <ibr.s.ahmat@gmail.com>
2022-06-09 22:17:11 +0300

    Add Books, Book, Categories, and AddNewBook components

---
+++ src/components/Books.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AddNewBook from './AddNewBook';
+import Book from './Book';
+
+const Books = () => (
+  <div>
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <AddNewBook />
+  </div>
+);
+
+export default Books;
